a54d9eea41064764fb4aa563a9a65e42480a91c5
Ken LEE <taw3004@gmail.com>
2019-12-08 04:46:24 +0800

    layout

--- pages/Screen1.js
+++ pages/Screen1.js
@@ -1,26 +1,50 @@
-//This is an example code for NavigationDrawer//
-import React, { Component } from 'react';
-//import react in our code.
-import { StyleSheet, View, Text } from 'react-native';
-// import all basic components
- 
-export default class Screen1 extends Component {
-  //Screen1 Component
+import React, {Component} from 'react';
+import {Text, View, Dimensions, ScrollView, StyleSheet} from 'react-native';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+
+const {width, height} = Dimensions.get('window');
+const SCREEN_WIDTH = width;
+const SCREEN_HEIGHT = height;
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+export default class MapScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      },
+      error: null
+    };
+  }
+
+  setRegion = () => ({latitude: this.state.region.latitude, longitude: this.state.region.longitude, latitudeDelta: LATITUDE_DELTA, longitudeDelta: LONGITUDE_DELTA});
+
   render() {
-    return (
-      <View style={styles.MainContainer}>
-        <Text style={{ fontSize: 23 }}> Screen 1 </Text>
-      </View>
-    );
+    return (<MapView provider={PROVIDER_GOOGLE} style={styles.map} scrollEnabled={true} zoomEnabled={true} pitchEnabled={true} rotateEnabled={true} initialRegion={this.state.region}>
+      <Marker title="This is a title" description="This is a description" coordinate={this.state.region}/>
+      <Marker title="This is setRegion" description="This is setRegion" coordinate={this.setRegion()}/>
+      {
+        !!this.state.region.latitude && !!this.state.region.longitude && <Marker coordinate={{
+              "latitude" : this.state.region.latitude,
+              "longitude" : this.state.region.longitude
+            }} title={"Your Location"}/>
+      }
+
+    </MapView>);
   }
 }
- 
+
 const styles = StyleSheet.create({
-  MainContainer: {
-    flex: 1,
-    paddingTop: 20,
-    alignItems: 'center',
-    marginTop: 50,
-    justifyContent: 'center',
-  },
+  map: {
+    flex: 1
+  }
 });
